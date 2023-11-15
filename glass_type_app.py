d08b8fd4d148058ddd84510483eb3ee18fac3465
ajuveriya <59317931+ajuveriya@users.noreply.github.com>
2023-11-15 07:11:26 +0530

    glass_type_app.py

--- glass_type_app.py
+++ glass_type_app.py
@@ -6,6 +6,16 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score
+
+# ML classifier Python modules
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 #from sklearn.model_selection import train_test_split, GridSearchCV
